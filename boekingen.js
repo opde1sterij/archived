d847bbc3cd46a9de3030fdf4f89cf258207a5afd
Op de eerste rij <opde1sterij@gmail.com>
2026-08-14 21:32:37 +0200

    Updated Boekingen and 1 other file

--- boekingen.js
+++ boekingen.js
@@ -20,12 +20,12 @@ function getFormData() {
                   // it to be appended to for each item in the loop
     if(elements[k].type === "checkbox"){ // special case for Edge's html collection
       if(elements[k].checked){
-        str = str + elements[k].value + ", "; // take the string and append 
-                                              // the current checked value to 
-                                              // the end of it, along with 
+        str = str + elements[k].value + ", "; // take the string and append
+                                              // the current checked value to
+                                              // the end of it, along with
                                               // a comma and a space
-        data[k] = str.slice(0, -2); // remove the last comma and space 
-                                  // from the  string to make the output 
+        data[k] = str.slice(0, -2); // remove the last comma and space
+                                  // from the string to make the output
                                   // prettier in the spreadsheet
       }
     } else if(elements[k].length){
@@ -37,31 +37,72 @@ function getFormData() {
       }
     }
   });
-
   // add form-specific values into the data
   data.formDataNameOrder = JSON.stringify(fields);
   data.formGoogleSheetName = form.dataset.sheet || "boekingen"; // default sheet name
   data.formGoogleSendEmail = form.dataset.email || ""; // no email by default
-
   return data;
 }
+
+var formLoadedAt = Date.now();
 
 function handleFormSubmit(event) {  // handles form submit withtout any jquery
   event.preventDefault();           // we are submitting via xhr below
   var data = getFormData();         // get the values submitted in the form
 
+  var shouldNotSubmit = data.website !== '';
+  delete data.website;
+
+  if (shouldNotSubmit) {
+    return;
+  }
+
+  if (Date.now() - formLoadedAt < 2000) {
+    return;
+  }
+
+  var buttons = event.target.querySelectorAll('button, input[type="submit"]');
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].disabled = true;
+  }
+
   var url = event.target.action;
   var xhr = new XMLHttpRequest();
   xhr.open('POST', url);
   xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhr.onreadystatechange = function() {
-      document.getElementById('boekingsbevestiging').style.display = 'none';
-      document.getElementById('boeking-verzonden').style.display = 'inline-block';
+  var handled = false;
+  function showError() {
+    if(handled) {
       return;
+    }
+    handled = true;
+    for (var i = 0; i < buttons.length; i++) {
+      buttons[i].disabled = false;
+    }
+    alert('Verzending niet gelukt. Probeer het opnieuw of neem contact met ons op via info@opde1sterij.nl');
+  }
+  xhr.onreadystatechange = function() {
+    if(xhr.readyState === 4 && !handled) {
+      try {
+        var result = JSON.parse(xhr.responseText);
+        if(result.result === 'success') {
+          handled = true;
+          document.getElementById('boekingsbevestiging').style.display = 'none';
+          document.getElementById('boeking-verzonden').style.display = 'inline-block';
+        } else {
+          showError();
+        }
+      } catch(error) {
+        showError();
+      }
+    }
   };
+  xhr.onerror = showError;
+  xhr.ontimeout = showError;
+  xhr.timeout = 20000;
   // url encode form data for sending as post data
   var encoded = Object.keys(data).map(function(k) {
-      return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
+    return encodeURIComponent(k) + '=' + encodeURIComponent(data[k])
   }).join('&')
   xhr.send(encoded);
 }
